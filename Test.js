1f7ec018e39d4bce278b7bac076411646d995b71
info-talha <infotectalha@gmail.com>
2019-03-19 16:36:16 +0500

    Test.js

---
+++ Test.js
@@ -0,0 +1,7 @@
+var resident = new Object();
+resident.name = "talha";
+resident.city = "bhalwal";
+resident.area = "manzoor hayat colony";
+resident.street = "A1";
+resident.dist = "Sargodha";
+console.log("My name is",resident.name,"from",resident.city,resident.area,"street",resident.street,"dist",resident.dist)
